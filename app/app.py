cb3fe5a0dcd90754e07ec67b37ab0696debfb36d
Bar Dadon <bdadon50@gmail.com>
2022-12-22 16:04:46 +0200

    Added bootstrap to Flask, HTML templates and a histogram plot

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from flask import Response
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
-from flask import Flask
+from flask import Flask, render_template
 import pandas as pd
 
 # Set default settings
@@ -13,34 +13,45 @@ plt.rcParams["figure.autolayout"] = True
 # Grab the processed rates DataFrame
 df = pd.read_csv('dags/processed_rates.csv', index_col = 'Unnamed: 0')
 dates = df.date.unique()
+symbols = df.symbol.values
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Welcome to the Bar's Forex Investing Platform"
+    return render_template('home.html', title = 'home')
 
 @app.route('/')
 @app.route('/plots')
 def plots():
-    return 'Enter a symbol in the URL to check a line chart'
+    return render_template('plots.html', title = 'plots', symbols= symbols)
 
 
 # Web Page #1 - Plot a line chart according to the symbol in the URL
 @app.route('/plots/<symbol>')
 def plot_png(symbol):
 
-   # Create a figure and an axis objects
-   fig = Figure()
-   axis = fig.add_subplot(1, 1, 1)
+   plt.style.use('ggplot')
+    
+    # Extract the rates of the symbol in the URL
+   rates = df[df.symbol == symbol].rate
 
-   # Extract the rates of the symbol in the URL
-   rates = df[df.symbol == symbol].rate.values
+   # Create a figure and an axis objects
+   fig, ax = plt.subplots(2,1, figsize = [15,8])
 
    # Create a line chart
-   axis.plot(dates, rates)
+   ax[0].plot(rates, dates,  marker = 'v', linestyle= '--' ,c = 'r')
+   ax[0].set_xlabel('Rate', size = 16)
+   ax[0].set_ylabel('Date', size = 16, rotation = 0)
+   ax[0].set_title(f'Line Chart - {symbol}', size = 20)
 
+   # Create a histogram plot
+   ax[1].hist(rates)
+   ax[1].set_xlabel('Rate', size = 16)
+   ax[0].set_ylabel('Density', size = 16, rotation = 0)
+   ax[1].set_title(f'Histogram - {symbol}', size = 20)
+   
    # Output the chart as PNG
    output = io.BytesIO()
    FigureCanvas(fig).print_png(output)
